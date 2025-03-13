7ed06ff8219794a6e332bb52de5995be7eb477bf
LavellM25 <Lavellmcgrone25@gmail.com>
2025-03-13 14:12:42 -0500

    project 5

---
+++ Bro Code Playlist (Javascript)/Example_5/project_interactive5.js
@@ -0,0 +1,6 @@
+console.log("Hello World"); // test that the console is receiving the .JS file
+
+/** This example we will explore type conversion.
+ * Type conversion is the change of a datatype from one form to another,
+ * i.e. strings, numbers, booleans, possibly being changed from numbers to strings, or vice versa.
+ */
