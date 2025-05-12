f7fc1de037f4f2284ca09fe12553023567ecf29e
Lavell <Lavellmcgrone25@gmail.com>
2025-05-11 20:02:11 -0500

    project 12

--- Bro Code Playlist (Javascript)/Example_12/project_interactive12.js
+++ Bro Code Playlist (Javascript)/Example_12/project_interactive12.js
@@ -1,2 +1,6 @@
 console.log("Hello World") // Check if the js code is rendering correct.
 
+/** Ternary Operator: This is a logic shortcut to using if and else statements.
+ *                    This helps assign a variable to a certain condition based on if a block of code is true or false.
+ *                    Example: condition ? codeIfTrue : codeIfFalse
+ */
